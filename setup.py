b66ec6893522c15f7ea119ae69f04234f1ae50aa
jkchen2 <jkchen2@illinois.edu>
2018-06-27 18:15:50 -0500

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nextrip',
-    version='1.0.0',
+    version='1.0.1',
     description='A NexTrip API wrapper',
     url='https://github.com/jkchen2/NexTriPy',
     author='jkchen2',
